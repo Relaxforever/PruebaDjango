a259fade1dcbfe715080830e4e8782ea89422334
Fidel Fernando Caicedo Castaño <FidelCaicedo@comfama.com.co>
2022-02-10 22:48:24 -0500

    Subiendo Cambios en Admin,SQlite y comentarios

--- djangointroductorio/settings.py
+++ djangointroductorio/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
